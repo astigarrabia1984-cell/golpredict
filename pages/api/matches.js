e70e8390dfed6f930070864ce104a9b9c3d6c594
astigarrabia1984-cell <astigarrabia1984@gmail.com>
2026-02-21 00:46:56 +0100

    Delete pages/api directory

--- pages/api/matches.js
+++
@@ -1,8 +0,0 @@
-export default async function handler(req, res) {
-  const response = await fetch("https://api.football-data.org/v4/matches", {
-    headers: { "X-Auth-Token": "8622f57039804f3fbf997840e90c8b18" }
-  });
-
-  const data = await response.json();
-  res.status(200).json(data);
-}
